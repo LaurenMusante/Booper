d44eae3a70f0209516b36f44ed2a4aa099ee204d
Lauren <Lauren.Musante@gmail.com>
2019-08-23 14:18:31 -0700

    Enable JavaScript to replace 1, 2, and 3 with corresponding replacements 'beep', 'boop', 'Im sorry'

--- js/scripts.js
+++ js/scripts.js
@@ -10,21 +10,23 @@ $(document).ready(function() {
       output = booper(input);
     }
     $("#output").text(returnArray);
+
 //BACK END
 function booper(input){
 var returnArray = [];
 var input = $("#input").val();
-    for (var i=0; i<input; i++) {
+    for (var i=0; i<=input; i++) {
       returnArray.push(i);
       console.log(returnArray)
-      if (returnArray.includes("3")){
-          returnArray[i] = "I'm sorry Dave. I'm afraid I cant do that.";
+
+      if (returnArray.includes(3)){
+          returnArray[i] += "I'm sorry Dave. I'm afraid I cant do that.";
           console.log("I'm sorry");
-        } else if (returnArray.includes("2")){
-          returnArray[i] = "Boop!"
+        } else if (returnArray.includes(2)){
+          returnArray[i] += "Boop!"
           console.log("Boop!")
-        } else if (returnArray.includes("1")){
-          returnarray[i].push = ("Beep!")
+        } else if (returnArray.includes(1)){
+          returnArray[i] += ("Beep!")
         }
     };
         return returnArray;
