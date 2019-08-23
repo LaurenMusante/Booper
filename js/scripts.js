bd7d26122c60e3016931c611c2d7bcf1567e270b
Lauren <Lauren.Musante@gmail.com>
2019-08-23 12:58:13 -0700

    Make returnArray actually return an array of numbers that increase by 1 each time, instead of returning an array of the same number repeated.

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,32 @@
-//USER INTERFACE USER INTERFACE USER INTERFACE USER INTERFACE
+//USER INTERFACE
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
     var input = parseInt($("#input").val());
-
+    var returnArray = [];
     if (input <1){
       alert("Enter a number greater than 0.")
     } else {
       output = booper(input);
     }
-    $("#output").text(booper());
-
-//BACKEND BACKEND BACKEND BACKEND BACKEND BACKEND BACKEND
+    $("#output").text(returnArray);
+//BACK END
 function booper(input){
-  var returnArray = [];
-  var input = $("#input").val();
-
-    for (var i=0; i<=returnArray.length; i++) {
-      returnArray.push(input);
-      console.log("here")
+var returnArray = [];
+var input = $("#input").val();
+    for (var i=0; i<input; i++) {
+      returnArray.push(i);
+      console.log(returnArray)
       if (returnArray.includes("3")){
           returnArray[i] = "I'm sorry Dave. I'm afraid I cant do that.";
           console.log("I'm sorry");
         } else if (returnArray.includes("2")){
           returnArray[i] = "Boop!"
           console.log("Boop!")
-
         } else if (returnArray.includes("1")){
           returnarray[i].push = ("Beep!")
         }
-        };
+    };
         return returnArray;
       };
     });
