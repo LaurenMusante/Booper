02e7d0befabc35af085b2a3d89d16245fac71ecf
Lauren <Lauren.Musante@gmail.com>
2019-08-24 14:07:43 -0700

    Fix for loop to find and replace all numbers containing a '1', '2', or '3' instead of only finding a replacing the actual numbers '1', '2', and '3'.

--- js/scripts.js
+++ js/scripts.js
@@ -2,37 +2,42 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-    var input = parseInt($("#input").val());
-    var returnArray = [];
 
+var input = parseInt($("#input").val());
+var returnArray = [];
     if (input <1){
       alert("Enter a number greater than 0.");
     } else {
       output = booper(input);
     }
+    $("#output").text(output);
+  });
 
-$("#output").text(returnArray);
-return returnArray;
+  $("#refresh").click(function(){
+    $("formOne").trigger("reset");
+  });
+});
 
 //BACK END
 var returnArray = [];
 var input = $("#input").val();
+
 function booper(input){
     for (var i=0; i<=input; i++) {
       returnArray.push(i);
       console.log(returnArray)
-
-      if (returnArray.includes(3)){
-          returnArray[i] = "I'm sorry Dave. I'm afraid I cant do that.";
-
-        } else if (returnArray.includes(2)){
-          returnArray[i] = "Boop!"
-
-        } else if (returnArray.includes(1)){
-          returnArray[i] = "Beep!"
-        }
     };
-      };
-    });
-    // return returnArray;
-  });
+
+      for (var i=0; i<returnArray.length; i++) {
+        if (returnArray[i].toString().includes(3)){
+        returnArray[i] = " I'm sorry Dave. I'm afraid I cant do that. ";
+
+        } else if (returnArray[i].toString().includes(2)){
+          returnArray[i] = " Boop! "
+
+        } else if (returnArray[i].toString().includes(1)){
+          returnArray[i] = " Beep! "
+        }
+    }
+      return returnArray;
+    };
