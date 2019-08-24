0131d5b644481930b449598b989628ae16caa120
Lauren <Lauren.Musante@gmail.com>
2019-08-24 15:09:01 -0700

    Fix indentation in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,26 +3,25 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
 
-var input = parseInt($("#input").val());
-var returnArray = [];
-    if (input <1){
-      alert("Enter a number greater than 0.");
-    } else {
-      output = booper(input);
-    }
-    $("#output").text(output);
+    var input = parseInt($("#input").val());
+      if (input <1){
+        alert("Enter a number greater than 0.");
+      } else {
+      var output = booper(input).join(", ");
+      }
+      $("#output").text(output);
   });
 
   $("#refresh").click(function(){
     $("formOne").trigger("reset");
   });
+
 });
 
 //BACK END
-var returnArray = [];
-var input = $("#input").val();
-
 function booper(input){
+  var returnArray = [];
+
     for (var i=0; i<=input; i++) {
       returnArray.push(i);
       console.log(returnArray)
@@ -30,14 +29,14 @@ function booper(input){
 
       for (var i=0; i<returnArray.length; i++) {
         if (returnArray[i].toString().includes(3)){
-        returnArray[i] = " I'm sorry Dave. I'm afraid I cant do that. ";
+        returnArray[i] = "I'm sorry Dave. I'm afraid I cant do that.";
 
         } else if (returnArray[i].toString().includes(2)){
-          returnArray[i] = " Boop! "
+          returnArray[i] = "Boop!"
 
         } else if (returnArray[i].toString().includes(1)){
-          returnArray[i] = " Beep! "
+          returnArray[i] = "Beep!"
         }
     }
-      return returnArray;
-    };
+    return returnArray;
+  };
